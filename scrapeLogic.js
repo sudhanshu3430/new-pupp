9957c473140f4307fc2fb6142921db1cd69ea6ac
Sudhanshu Singh <sudhanshu3430@gmail.com>
2024-11-08 14:26:33 +0530

    new

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,29 +17,39 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://www.amazon.in/BRUTON-Lite-Sport-Shoes-Running/dp/B0DHH7TMQ1");
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+    // // Set screen size
+    // await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    // await page.type(".search-box__input", "automate beyond recorder");
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    // // Wait and click on first result
+    // const searchResultSelector = ".search-box__link";
+    // await page.waitForSelector(searchResultSelector);
+    // await page.click(searchResultSelector);
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    // // Locate the full title with a unique string
+    // const textSelector = await page.waitForSelector(
+    //   "text/Customize and automate"
+    // );
+    // const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    // // Print the full title
+    // const logStatement = `The title of this blog post is ${fullTitle}`;
+    // console.log(logStatement);
+            // Extract product title
+            const productTitle = await page.$eval('#title',el => el.innerText.trim());
+
+            // Extract product price
+            const productPrice = await page.$eval('.a-price .a-offscreen', el => el.innerText.trim());
+            
+            // Extract product description
+            const productDesc = await page.$$eval('.a-list-item', items => {
+                return items.map(item => item.innerText.trim()).filter(text => text); // Extract inner text and filter out empty entries
+            });
+    res.send(productTitle, productDesc, productPrice);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
